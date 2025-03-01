16ba4bad25c544da334f1d7f81e3fabb33136a8d
donald-okara <donaldokara123@gmail.com>
2025-03-01 15:18:06 +0300

    feat: Implement Pull-to-Refresh in User Library Screen
    -   Added `PullToRefreshBox` to `UserLibraryScreen` to allow users to refresh bookshelf data by pulling down.
    -   Implemented `refreshAction` in `UserLibraryViewModel` to trigger data refresh when pull-to-refresh is activated.
    -   Updated `UserLibraryScreen` to use `mutableStateOf` to manage the refreshing state.
    -   Added a callback function `onRefresh` to `PullToRefreshBox` to trigger data refresh.
    -   Added call to fetch user book shelves in `refreshAction` function.
    - Added `onRefreshComplete` callback to set `isRefreshing` to false.'

--- feature-bookshelf/src/main/java/ke/don/feature_bookshelf/presentation/screens/user_library/UserLibraryViewModel.kt
+++ feature-bookshelf/src/main/java/ke/don/feature_bookshelf/presentation/screens/user_library/UserLibraryViewModel.kt
@@ -1,8 +1,11 @@
 package ke.don.feature_bookshelf.presentation.screens.user_library
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ke.don.common_datasource.remote.domain.repositories.BookshelfRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +13,15 @@ class UserLibraryViewModel @Inject constructor(
     private val bookshelfRepository: BookshelfRepository
 ) : ViewModel() {
     val userLibraryState = bookshelfRepository.userLibraryState
+
+    fun refreshAction(onRefreshComplete: () -> Unit){
+        viewModelScope.launch {
+            try {
+                bookshelfRepository.fetchUserBookShelves()
+            } finally {
+                onRefreshComplete()
+            }
+        }
+    }
 
 }
